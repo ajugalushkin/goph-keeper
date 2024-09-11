6fede4ee43cb6d775bafa9fb2cbbe13b1f24501f
ajugalushkin <ajugalushkin@gmail.com>
2024-09-11 19:15:31 +0300

    feat: add new tests

--- client/cmd/auth_login_test.go
+++ client/cmd/auth_login_test.go
@@ -1,6 +1,13 @@
 package cmd
 
 import (
+	"context"
+	"github.com/ajugalushkin/goph-keeper/client/internal/app"
+	"github.com/ajugalushkin/goph-keeper/client/internal/config"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"log/slog"
+	"os"
 	"testing"
 
 	"github.com/spf13/cobra"
@@ -43,4 +50,92 @@ func TestErrorMarkingEmailFlagRequired(t *testing.T) {
 	if err == nil {
 		t.Errorf("Expected an error, but got none")
 	}
+}
+
+type MockAuthClient struct {
+	mock.Mock
+}
+
+func (m *MockAuthClient) Login(ctx context.Context, email, password string) (string, error) {
+	args := m.Called(ctx, email, password)
+	return args.String(0), args.Error(1)
+}
+
+type MockTokenStorage struct {
+	mock.Mock
+}
+
+func (m *MockTokenStorage) Save(token string) error {
+	args := m.Called(token)
+	return args.Error(0)
+}
+
+func TestLoginWithValidEmailAndPassword(t *testing.T) {
+	email := "test@example.com"
+	password := "testpassword"
+
+	mockAuthClient := &MockAuthClient{}
+	mockAuthClient.On("Login", context.Background(), email, password).Return("test_token", nil)
+
+	token, err := mockAuthClient.Login(context.Background(), email, password)
+	assert.Nil(t, err, "Expected no error, but got %v", err)
+	assert.Equal(t, "test_token", token, "Expected token 'test_token', but got %s", token)
+
+	mockTokenStorage := &MockTokenStorage{}
+	mockTokenStorage.On("Save", token).Return(nil)
+
+	err = mockTokenStorage.Save(token)
+	assert.Nil(t, err, "Expected no error, but got %v", err)
+
+	mockAuthClient.AssertExpectations(t)
+	mockTokenStorage.AssertExpectations(t)
+}
+
+func TestLoginWithInvalidEmailFormat(t *testing.T) {
+	const op = "client.auth.login.run"
+	log := slog.New(
+		slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+	)
+	log.With("op", op)
+
+	email := "invalid_email"
+	password := "testpassword"
+
+	cfg := config.GetInstance().Config
+	authClient := app.NewAuthClient(app.GetAuthConnection(log, cfg.Client))
+
+	_, err := authClient.Login(context.Background(), email, password)
+	assert.Error(t, err, "Expected error for invalid email format")
+}
+func TestLoginWithEmptyEmail(t *testing.T) {
+	const op = "client.auth.login.run"
+	log := slog.New(
+		slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+	)
+	log.With("op", op)
+
+	email := ""
+	password := "testpassword"
+
+	cfg := config.GetInstance().Config
+	authClient := app.NewAuthClient(app.GetAuthConnection(log, cfg.Client))
+
+	_, err := authClient.Login(context.Background(), email, password)
+	assert.Error(t, err, "Expected error for empty email")
+}
+func TestLoginWithEmptyPassword(t *testing.T) {
+	const op = "client.auth.login.run"
+	log := slog.New(
+		slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+	)
+	log.With("op", op)
+
+	email := "test@example.com"
+	password := ""
+
+	cfg := config.GetInstance().Config
+	authClient := app.NewAuthClient(app.GetAuthConnection(log, cfg.Client))
+
+	_, err := authClient.Login(context.Background(), email, password)
+	assert.Error(t, err, "Expected error for empty password")
 }
