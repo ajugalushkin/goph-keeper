f43e804dc9a84fb3cda9866610ff9f91730fc6ea
ajugalushkin <ajugalushkin@gmail.com>
2024-09-15 11:38:48 +0300

    feat: add test

--- client/cmd/keep/list/keep_list_test.go
+++ client/cmd/keep/list/keep_list_test.go
@@ -1,11 +1,14 @@
 package list
 
 import (
+	"context"
 	"errors"
 	"log/slog"
 	"os"
+	"strings"
 	"testing"
 
+	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 
@@ -14,6 +17,23 @@ import (
 	"github.com/ajugalushkin/goph-keeper/client/internal/logger"
 	v1 "github.com/ajugalushkin/goph-keeper/gen/keeper/v1"
 )
+
+// Command creation with correct use and short description
+func TestNewCommandCreation(t *testing.T) {
+	cmd := NewCommand()
+
+	if cmd.Use != "list" {
+		t.Errorf("expected command use to be 'list', got %s", cmd.Use)
+	}
+
+	if cmd.Short != "List secrets" {
+		t.Errorf("expected command short description to be 'List secrets', got %s", cmd.Short)
+	}
+
+	if cmd.RunE == nil {
+		t.Error("expected command RunE to be set, got nil")
+	}
+}
 
 func TestKeepListRunE_EmptyList(t *testing.T) {
 	logger.InitLogger(slog.New(slog.NewJSONHandler(os.Stdout, nil)),
@@ -68,4 +88,59 @@ func TestNewCommand_NoItemsFound(t *testing.T) {
 	cmd.SetArgs([]string{"list"})
 	err := cmd.Execute()
 	assert.EqualError(t, err, expectedErr.Error())
+}
+
+// Successful execution of keepListRunE function
+func TestKeepListRunEErrorHandling(t *testing.T) {
+	logger.InitLogger(slog.New(slog.NewJSONHandler(os.Stdout, nil)),
+		&config.Config{Env: "dev"})
+
+	// Create a new command
+	cmd := NewCommand()
+
+	// Mock the client
+	mockClient := mocks.NewKeeperClient(t)
+	initClient(mockClient)
+
+	// Mock an error response from ListItems
+	mockClient.On("ListItems", mock.Anything, mock.Anything).Return(nil, errors.New("mocked error"))
+
+	// Call the function under test
+	err := keepListRunE(cmd, []string{})
+
+	// Check if the error is handled correctly
+	if err == nil {
+		t.Error("expected an error, got nil")
+	}
+}
+
+// Logging of errors during secret listing
+func TestKeepListRunErrorLogging(t *testing.T) {
+	logger.InitLogger(slog.New(slog.NewJSONHandler(os.Stdout, nil)),
+		&config.Config{Env: "dev"})
+
+	// Create a fake cobra.Command
+	fakeCmd := &cobra.Command{}
+
+	mockError := errors.New("simulated error")
+	mockClient := mocks.NewKeeperClient(t)
+	mockClient.On(
+		"ListItems",
+		context.Background(),
+		&v1.ListItemsRequestV1{},
+	).Return(&v1.ListItemsResponseV1{}, mockError)
+	initClient(mockClient)
+
+	// Call the function under test
+	err := keepListRunE(fakeCmd, []string{})
+
+	// Check if the error is logged
+	if !strings.Contains(err.Error(), mockError.Error()) {
+		t.Errorf("expected error to be logged, got: %s", err.Error())
+	}
+
+	// Check if the error is returned
+	if err == nil || err.Error() != mockError.Error() {
+		t.Errorf("expected error to be returned, got: %v", err)
+	}
 }
