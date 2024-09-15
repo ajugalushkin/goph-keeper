12925d64a084dcc1aa96f62f9c8d1155832a8428
ajugalushkin <ajugalushkin@gmail.com>
2024-09-15 12:20:24 +0300

    feat: add new tests

--- client/cmd/keep/update/creds/keep_update_credentials_test.go
+++ client/cmd/keep/update/creds/keep_update_credentials_test.go
@@ -1,6 +1,7 @@
 package creds
 
 import (
+	"context"
 	"log/slog"
 	"os"
 	"testing"
@@ -9,8 +10,12 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/ajugalushkin/goph-keeper/client/internal/app/mocks"
 	"github.com/ajugalushkin/goph-keeper/client/internal/config"
 	"github.com/ajugalushkin/goph-keeper/client/internal/logger"
+	"github.com/ajugalushkin/goph-keeper/client/internal/secret"
+	"github.com/ajugalushkin/goph-keeper/client/internal/vaulttypes"
+	v1 "github.com/ajugalushkin/goph-keeper/gen/keeper/v1"
 )
 
 func TestKeepUpdateCredsRunE_SecretNameNotProvided(t *testing.T) {
@@ -78,4 +83,94 @@ func TestKeepUpdateCredsRunE_EmptyName(t *testing.T) {
 	err = keepUpdateCredsRunE(cmd, nil)
 	require.Error(t, err)
 	assert.EqualError(t, err, "name is required")
+}
+
+// Successfully reads command-line flags for name, login, and password
+func TestKeepUpdateCredsRunE_SuccessfulFlagRead(t *testing.T) {
+	logger.InitLogger(slog.New(slog.NewJSONHandler(os.Stdout, nil)), &config.Config{Env: "dev"})
+
+	cmd := &cobra.Command{}
+	cmd.Flags().String("name", "testName", "name of the secret")
+	cmd.Flags().String("login", "testLogin", "login for the secret")
+	cmd.Flags().String("password", "testPassword", "password for the secret")
+
+	mockClient := mocks.NewKeeperClient(t)
+
+	credentials := vaulttypes.Credentials{
+		Login:    "testLogin",
+		Password: "testPassword",
+	}
+
+	content, err := secret.EncryptSecret(credentials)
+	assert.NoError(t, err)
+
+	mockClient.On("UpdateItem", context.Background(), &v1.UpdateItemRequestV1{
+		Name:    "testName",
+		Content: content,
+	}).Return(&v1.UpdateItemResponseV1{Name: "testName", Version: "1"}, nil)
+	initClient(mockClient)
+
+	err = keepUpdateCredsRunE(cmd, []string{})
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+}
+
+// Command-line flag for name is missing or empty
+func TestKeepUpdateCredsRunE_MissingNameFlag(t *testing.T) {
+	logger.InitLogger(slog.New(slog.NewJSONHandler(os.Stdout, nil)), &config.Config{Env: "dev"})
+
+	cmd := &cobra.Command{}
+	cmd.Flags().String("login", "testLogin", "login for the secret")
+	cmd.Flags().String("password", "testPassword", "password for the secret")
+
+	err := keepUpdateCredsRunE(cmd, []string{})
+	if err == nil || err.Error() != "error reading secret name" {
+		t.Fatalf("Expected 'name is required' error, got %v", err)
+	}
+}
+
+// Creates Credentials object with provided login and password
+func TestKeepUpdateCredsRunE_CreatesCredentials(t *testing.T) {
+	logger.InitLogger(slog.New(slog.NewJSONHandler(os.Stdout, nil)), &config.Config{Env: "dev"})
+
+	cmd := &cobra.Command{}
+	cmd.Flags().String("name", "testName", "name of the secret")
+	cmd.Flags().String("login", "testLogin", "login for the secret")
+	cmd.Flags().String("password", "testPassword", "password for the secret")
+
+	err := keepUpdateCredsRunE(cmd, []string{})
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+}
+
+// Command-line flag for login is missing or empty
+func TestKeepUpdateCredsRunE_LoginFlagMissingOrEmpty(t *testing.T) {
+	logger.InitLogger(slog.New(slog.NewJSONHandler(os.Stdout, nil)), &config.Config{Env: "dev"})
+
+	cmd := &cobra.Command{}
+	cmd.Flags().String("name", "testName", "name of the secret")
+	cmd.Flags().String("login", "", "login for the secret")
+	cmd.Flags().String("password", "testPassword", "password for the secret")
+
+	err := keepUpdateCredsRunE(cmd, []string{})
+	if err == nil || err.Error() != "login is required" {
+		t.Fatalf("Expected error 'login is required', got %v", err)
+	}
+}
+
+// Command-line flag for password is missing or empty
+func TestKeepUpdateCredsRunE_PasswordFlagMissingOrEmpty(t *testing.T) {
+	logger.InitLogger(slog.New(slog.NewJSONHandler(os.Stdout, nil)), &config.Config{Env: "dev"})
+
+	cmd := &cobra.Command{}
+	cmd.Flags().String("name", "testName", "name of the secret")
+	cmd.Flags().String("login", "testLogin", "login for the secret")
+	cmd.Flags().String("password", "", "password for the secret")
+
+	err := keepUpdateCredsRunE(cmd, []string{})
+	if err == nil || err.Error() != "password is required" {
+		t.Fatalf("Expected error 'password is required', got %v", err)
+	}
 }
