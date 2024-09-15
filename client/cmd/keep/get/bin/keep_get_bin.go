bcc98e7465294b455c79c6ceaecafb58764dc91b
ajugalushkin <ajugalushkin@gmail.com>
2024-09-15 10:57:26 +0300

    feat: add new tests

--- client/cmd/keep/get/bin/keep_get_bin.go
+++ client/cmd/keep/get/bin/keep_get_bin.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/ajugalushkin/goph-keeper/client/internal/app"
 	"github.com/ajugalushkin/goph-keeper/client/internal/app/keeper"
 	"github.com/ajugalushkin/goph-keeper/client/internal/config"
 	"github.com/ajugalushkin/goph-keeper/client/internal/secret"
@@ -19,6 +20,14 @@ import (
 	"github.com/ajugalushkin/goph-keeper/client/internal/vaulttypes"
 )
 
+var keepGetBin = &cobra.Command{
+	Use:   "bin",
+	Short: "Get secret",
+	RunE:  keepGetBinRunE,
+}
+
+var client app.KeeperClient
+
 // NewCommand creates a new cobra.Command for retrieving a secret file from the goph-keeper service.
 // The command is configured to accept two flags: "name" and "path".
 // The "name" flag specifies the name of the secret to be retrieved.
@@ -26,41 +35,22 @@ import (
 // If either flag is not provided, an error will be logged.
 // The command runs the keepGetBinRun function to handle the retrieval process.
 func NewCommand() *cobra.Command {
-	const op = "keep_get_bin"
-
-	cmd := &cobra.Command{
-		Use:   "bin",
-		Short: "Get secret",
-		Run:   keepGetBinRun,
-	}
-
-	cmd.Flags().String("name", "", "Secret name")
-	if err := cmd.MarkFlagRequired("name"); err != nil {
-		slog.Error("Error setting flag: ",
-			slog.String("op", op),
-			slog.String("error", err.Error()))
-	}
-
-	cmd.Flags().String("path", "", "Path to downloaded secret")
-	if err := cmd.MarkFlagRequired("path"); err != nil {
-		slog.Error("Error setting flag: ",
-			slog.String("op", op),
-			slog.String("error", err.Error()))
-	}
-
-	return cmd
+	return keepGetBin
 }
 
-// keepGetBinRun is the function that handles the retrieval of a secret file from the goph-keeper service.
-// It uses gRPC to communicate with the server and streams the file data to a local file.
+// keepGetBinRunE is a function that handles the retrieval of a secret file from the goph-keeper service.
+// It reads the secret name and path from command flags, initializes the Keeper client if not already done,
+// requests the file stream from the goph-keeper service, decrypts the secret file content,
+// and streams the file chunks to a local file.
 //
 // Parameters:
-// - cmd: A pointer to the cobra.Command object representing the command being executed.
-// - args: A slice of strings containing any additional arguments passed to the command.
+// - cmd: A pointer to the Cobra command object representing the "get bin" command.
+// - args: An array of strings representing any additional arguments passed to the command.
 //
-// Return:
-// - This function does not return any value.
-func keepGetBinRun(cmd *cobra.Command, args []string) {
+// Returns:
+// - An error if any error occurs during the retrieval process.
+// - nil if the retrieval is successful.
+func keepGetBinRunE(cmd *cobra.Command, args []string) error {
 	const op = "keep_get_bin"
 	log := logger.GetLogger().With("op", op)
 
@@ -68,45 +58,52 @@ func keepGetBinRun(cmd *cobra.Command, args []string) {
 	name, err := cmd.Flags().GetString("name")
 	if err != nil {
 		log.Error("Error reading secret name: ", slog.String("error", err.Error()))
-		return
+		return fmt.Errorf("error reading secret name")
+	}
+	if name == "" {
+		return fmt.Errorf("secret name is required")
 	}
 
 	// Read the path where the downloaded secret will be saved from the command flags
 	path, err := cmd.Flags().GetString("path")
 	if err != nil {
 		log.Error("Error reading secret path: ", slog.String("error", err.Error()))
-		return
+		return fmt.Errorf("error reading secret path")
+	}
+	if path == "" {
+		return fmt.Errorf("secret path is required")
 	}
 
-	// Load the authentication token_cache from the token_cache storage
-	token, err := token_cache.GetToken().Load()
-	if err != nil {
-		log.Error("Error loading token_cache: ", slog.String("error", err.Error()))
-		return
+	// If the Keeper client is not initialized, load the authentication token_cache from storage and create a new client.
+	if client == nil {
+		// Load the authentication token_cache from storage.
+		token, err := token_cache.GetToken().Load()
+		if err != nil {
+			return err
+		}
+		// Create a new Keeper client using the provided configuration and token_cache.
+		cfg := config.GetConfig().Client
+		client = keeper.NewKeeperClient(keeper.GetKeeperConnection(log, cfg.Address, token))
 	}
-
-	// Create a new gRPC client for interacting with the goph-keeper service
-	cfg := config.GetConfig().Client
-	keeperClient := keeper.NewKeeperClient(keeper.GetKeeperConnection(log, cfg.Address, token))
-
 	// Request the file stream from the goph-keeper service
-	stream, err := keeperClient.GetFile(context.Background(), name)
+	stream, err := client.GetFile(context.Background(), name)
 	if err != nil {
 		log.Error("Error getting file stream: ", slog.String("error", err.Error()))
-		return
+		return err
 	}
 
 	// Receive the file information from the stream
 	req, err := stream.Recv()
 	if err != nil {
 		log.Error("Error getting file info: ", slog.String("error", err.Error()))
-		return
+		return err
 	}
 
 	// Decrypt the secret file content
 	respSecret, err := secret.DecryptSecret(req.GetContent())
 	if err != nil {
 		log.Error("Failed to decrypt secret: ", slog.String("error", err.Error()))
+		return err
 	}
 
 	// Extract the file information from the decrypted secret
@@ -116,6 +113,7 @@ func keepGetBinRun(cmd *cobra.Command, args []string) {
 	newFile, err := os.Create(filepath.Join(path, fileInfo.FileName))
 	if err != nil {
 		log.Error("Error creating file: ", slog.String("error", err.Error()))
+		return err
 	}
 	defer newFile.Close()
 
@@ -127,17 +125,42 @@ func keepGetBinRun(cmd *cobra.Command, args []string) {
 				break
 			}
 			log.Error("Error getting file chunk: ", slog.String("error", err.Error()))
-			return
+			return err
 		}
 		chunk := req.GetChunkData()
 
 		_, err = newFile.Write(chunk)
 		if err != nil {
 			log.Error("Error add chunk to file: ", slog.String("error", err.Error()))
-			return
+			return err
 		}
 	}
 
 	// Print a success message
 	fmt.Printf("file downloaded: %s\n", path)
+	return nil
+}
+
+// getBinCmdFlags sets up the command line flags for the "get bin" command.
+// It accepts two flags: "name" and "path".
+//
+// The "name" flag specifies the name of the secret to be retrieved.
+// The "path" flag specifies the path where the downloaded secret will be saved.
+//
+// Parameters:
+// - cmd: A pointer to the Cobra command object representing the "get bin" command.
+//
+// Returns:
+// - None.
+func getBinCmdFlags(cmd *cobra.Command) {
+	cmd.Flags().String("name", "", "Secret name")
+	cmd.Flags().String("path", "", "Path to downloaded secret")
+}
+
+func init() {
+	getBinCmdFlags(keepGetBin)
+}
+
+func initClient(newClient app.KeeperClient) {
+	client = newClient
 }
---
+++ client/cmd/keep/get/bin/keep_get_bin_test.go
@@ -0,0 +1,63 @@
+package bin
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"testing"
+
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+
+	"github.com/ajugalushkin/goph-keeper/client/internal/app/mocks"
+	"github.com/ajugalushkin/goph-keeper/client/internal/config"
+	"github.com/ajugalushkin/goph-keeper/client/internal/logger"
+)
+
+func TestKeepGetBinRunE_NoSecretName(t *testing.T) {
+	logger.InitLogger(slog.New(slog.NewJSONHandler(os.Stdout, nil)), &config.Config{
+		Env: "dev",
+	})
+
+	cmd := &cobra.Command{}
+	cmd.Flags().Set("name", "")
+	cmd.Flags().Set("path", "/tmp")
+
+	err := keepGetBinRunE(cmd, nil)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "error reading secret name")
+}
+func TestKeepGetBinRunE_SecretPathNotProvided(t *testing.T) {
+	logger.InitLogger(slog.New(slog.NewJSONHandler(os.Stdout, nil)), &config.Config{
+		Env: "dev",
+	})
+
+	cmd := NewCommand()
+	cmd.Flags().Set("name", "test-secret")
+	cmd.Flags().Set("path", "")
+
+	err := keepGetBinRunE(cmd, nil)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "secret path is required")
+}
+
+func TestKeepGetBinRunE_NonExistentSecretName(t *testing.T) {
+	logger.InitLogger(slog.New(slog.NewJSONHandler(os.Stdout, nil)), &config.Config{
+		Env: "dev",
+	})
+
+	cmd := NewCommand()
+	cmd.Flags().Set("name", "non-existent-secret")
+	cmd.Flags().Set("path", "/tmp")
+
+	mockClient := mocks.NewKeeperClient(t)
+	mockClient.On("GetFile", mock.Anything, "non-existent-secret").Return(nil, fmt.Errorf("secret not found"))
+
+	initClient(mockClient)
+
+	err := keepGetBinRunE(cmd, nil)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "secret not found")
+	mockClient.AssertExpectations(t)
+}
