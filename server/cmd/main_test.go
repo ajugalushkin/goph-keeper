2feb8ddbdcc41ecccc43bccd4c057999d5ca13ac
ajugalushkin <ajugalushkin@gmail.com>
2024-09-15 14:51:17 +0300

    feat: add new test

---
+++ server/cmd/main_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log/slog"
+	"testing"
+)
+
+func TestSetupLogger_DevEnvironment(t *testing.T) {
+	env := "dev"
+
+	logger := setupLogger(env)
+
+	if logger == nil {
+		t.Fatalf("Expected a logger instance, but got nil")
+	}
+
+	handler := logger.Handler()
+	if handler == nil {
+		t.Fatalf("Expected a handler, but got nil")
+	}
+
+	_, ok := handler.(*slog.TextHandler)
+	if !ok {
+		t.Fatalf("Expected a TextHandler, but got %T", handler)
+	}
+
+	//options := textHandler.GetOptions()
+	//if options.Level != expectedLogLevel {
+	//	t.Fatalf("Expected log level %v, but got %v", expectedLogLevel, options.Level)
+	//}
+}
+
+func TestSetupLogger_Prod(t *testing.T) {
+	const envProd = "prod"
+
+	logger := setupLogger(envProd)
+
+	if logger == nil {
+		t.Fatal("logger is nil")
+	}
+
+	handler := logger.Handler()
+	if handler == nil {
+		t.Fatal("logger handler is nil")
+	}
+
+	_, ok := handler.(*slog.TextHandler)
+	if !ok {
+		t.Fatalf("unexpected handler type: %T", handler)
+	}
+
+	//options := textHandler.Opts()
+	//if options.Level != expectedLogLevel {
+	//	t.Errorf("unexpected log level: got %v, want %v", options.Level, expectedLogLevel)
+	//}
+}
