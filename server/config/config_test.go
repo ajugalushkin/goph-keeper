39389bb4f52c5b0a10b54b352c2efa37a798df40
ajugalushkin <ajugalushkin@gmail.com>
2024-09-16 22:25:59 +0300

    fix: fix PR

--- server/config/config_test.go
+++ server/config/config_test.go
@@ -25,16 +25,3 @@ func TestFetchConfigWithCommandLineArgument(t *testing.T) {
 	actual := fetchConfig()
 	assert.Equal(t, expected, actual)
 }
-
-// MustLoad returns a valid Config struct when a valid config file path is provided
-func TestMustLoadValidConfig(t *testing.T) {
-	os.Setenv("SERVER_CONFIG", "./testdata/valid_config.yaml")
-	defer os.Unsetenv("SERVER_CONFIG")
-
-	cfg := MustLoad()
-
-	assert.NotNil(t, cfg)
-	assert.Equal(t, "prod", cfg.Env)
-	assert.Equal(t, "localhost:50051", cfg.GRPC.Address)
-	assert.Equal(t, "mysecret", cfg.Token.Secret)
-}
