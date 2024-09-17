89f984614010707d79db557fbd369e015a11e86c
ajugalushkin <ajugalushkin@gmail.com>
2024-09-17 16:19:52 +0300

    feat: refactor code

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
+	"github.com/ajugalushkin/goph-keeper/server/config"
+	"github.com/ajugalushkin/goph-keeper/server/internal/app"
 	"log/slog"
 	"os"
 	"os/signal"
 	"syscall"
-
-	config2 "github.com/ajugalushkin/goph-keeper/server/config"
-	"github.com/ajugalushkin/goph-keeper/server/internal/app"
 )
 
 var (
@@ -15,49 +14,20 @@ var (
 	buildDate    = "N/A"
 )
 
-var application *app.App
-var cfg *config2.Config
-
 // main is the entry point of the Goph-Keeper server application.
-// It initializes the application configuration, sets up a logger, starts the gRPC server,
-// and handles graceful shutdowns.
+// It initializes the application configuration, sets up a logger based on the environment,
+// creates a new application instance, runs the gRPC server, waits for OS signals for graceful shutdown,
+// and logs the application's start and stop events.
 func main() {
-	if cfg == nil {
-		cfg = config2.MustLoad()
-	}
-
+	cfg := config.MustLoad()
 	// Set up a logger based on the application environment.
 	log := setupLogger(cfg.Env)
 
-	// Log the start of the application with its environment, build version, and build date.
-	log.Info(
-		"starting application",
-		slog.String("env", cfg.Env),
-		slog.String("build version", buildVersion),
-		slog.String("build date", buildDate),
-	)
+	application := initApp(log, cfg)
 
-	// Create a new application instance with the configured logger and configuration.
-	if application == nil {
-		application = app.New(log, cfg)
-	}
+	run(application)
 
-	// Run the gRPC server in a separate goroutine.
-	go application.GRPCSrv.MustRun()
-
-	// Set up a channel to receive OS signals for graceful shutdown.
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-
-	// Wait for a signal to stop the application.
-	sign := <-stop
-	log.Info("stopping application", slog.String("signal", sign.String()))
-
-	// Stop the gRPC server.
-	application.GRPCSrv.Stop()
-
-	// Log the successful stop of the application.
-	log.Info("application stopped")
+	waitSignal(log, application)
 }
 
 // setupLogger initializes and configures a slog.Logger based on the provided environment.
@@ -91,10 +61,60 @@ func setupLogger(Env string) *slog.Logger {
 	return log
 }
 
-func initApp(newApp *app.App) {
-	application = newApp
+// run starts the gRPC server in a separate goroutine.
+//
+// The function takes an instance of the application, which contains the gRPC server.
+// It runs the gRPC server in a separate goroutine using the MustRun method of the gRPC server.
+// This allows the main function to continue executing without waiting for the gRPC server to finish.
+//
+// Parameters:
+// - application: A pointer to an instance of the application struct, which contains the gRPC server.
+func run(application *app.App) {
+	// Run the gRPC server in a separate goroutine.
+	go application.GRPCSrv.MustRun()
 }
 
-func initConfig(newCfg *config2.Config) {
-	cfg = newCfg
+// waitSignal listens for OS signals to gracefully shut down the application.
+// It sets up a channel to receive SIGINT, SIGTERM, and SIGQUIT signals, waits for a signal,
+// logs the received signal, stops the gRPC server, and logs the successful shutdown of the application.
+//
+// Parameters:
+// - log: A pointer to a slog.Logger instance for logging events.
+// - application: A pointer to an instance of the application struct, which contains the gRPC server.
+func waitSignal(log *slog.Logger, application *app.App) {
+	// Set up a channel to receive OS signals for graceful shutdown.
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	// Wait for a signal to waitSignal the application.
+	sign := <-stop
+	log.Info("stopping application", slog.String("signal", sign.String()))
+
+	// Stop the gRPC server.
+	application.GRPCSrv.Stop()
+
+	// Log the successful waitSignal of the application.
+	log.Info("application stopped")
+}
+
+// initApp initializes and returns a new application instance with the provided logger and configuration.
+// It logs the start of the application with its environment, build version, and build date.
+//
+// Parameters:
+// - log: A pointer to a slog.Logger instance for logging events.
+// - cfg: A pointer to a config2.Config instance containing the application's configuration.
+//
+// Returns:
+// - A pointer to a new instance of the app.App struct, which contains the gRPC server.
+func initApp(log *slog.Logger, cfg *config.Config) *app.App {
+	// Log the start of the application with its environment, build version, and build date.
+	log.Info(
+		"starting application",
+		slog.String("env", cfg.Env),
+		slog.String("build version", buildVersion),
+		slog.String("build date", buildDate),
+	)
+
+	// Create a new application instance with the configured logger and configuration.
+	return app.New(log, cfg)
 }
--- server/cmd/main_test.go
+++ server/cmd/main_test.go
@@ -1,9 +1,56 @@
 package main
 
 import (
+	testCfg "github.com/ajugalushkin/goph-keeper/server/config"
+	"github.com/ajugalushkin/goph-keeper/server/internal/app"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
 	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
 	"testing"
+	"time"
 )
+
+func TestMain(m *testing.M) {
+	os.Mkdir("test", 0777)
+	newConfig := testCfg.Config{
+		Env:     "dev",
+		Storage: testCfg.Storage{Path: "postgresql://praktikum:pass@localhost:5432/goph_keeper?sslmode=disable"},
+		GRPC: testCfg.GRPC{
+			Address: "localhost:50051",
+			Timeout: time.Hour,
+		},
+		Token: testCfg.Token{
+			TTL:    time.Hour,
+			Secret: "secret",
+		},
+		Minio: testCfg.Minio{
+			Endpoint: "localhost:9000",
+			Username: "testuser",
+			Password: "testpassword",
+			SSL:      false,
+			Bucket:   "testbucket",
+		},
+	}
+	yamlData, err := yaml.Marshal(&newConfig)
+	if err != nil {
+		return
+	}
+	filePath := "test/config.yaml"
+	err = os.WriteFile(filePath, yamlData, 0644)
+	if err != nil {
+		return
+	}
+	os.Setenv("SERVER_CONFIG", filePath)
+
+	exitcode := m.Run()
+
+	os.RemoveAll("test")
+	os.Clearenv()
+	os.Exit(exitcode)
+}
 
 func TestSetupLogger_DevEnvironment(t *testing.T) {
 	env := "dev"
@@ -42,5 +89,70 @@ func TestSetupLogger_Prod(t *testing.T) {
 	_, ok := handler.(*slog.TextHandler)
 	if !ok {
 		t.Fatalf("unexpected handler type: %T", handler)
+	}
+}
+
+func TestInitApp_NilLogger(t *testing.T) {
+	cfg := testCfg.MustLoad()
+
+	// Set up a nil logger.
+	var log *slog.Logger
+
+	// Expect a panic when initializing the application with a nil logger.
+	assert.Panics(t, func() {
+		initApp(log, cfg)
+	}, "expected initApp to panic with a nil logger")
+}
+
+func TestInitApp_NilConfig(t *testing.T) {
+	// Set up a logger for the test.
+	log := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	// Set up a nil configuration for the test.
+	var cfg *testCfg.Config
+
+	// Expect a panic when initializing the application with a nil configuration.
+	assert.Panics(t, func() {
+		initApp(log, cfg)
+	}, "expected initApp to panic with a nil configuration")
+}
+
+func TestRun_NilApplication(t *testing.T) {
+	// Create a nil application pointer.
+	var application *app.App
+
+	// Expect a panic when running the application with a nil pointer.
+	assert.Panics(t, func() {
+		run(application)
+	}, "expected run to panic with a nil application")
+}
+
+func TestWaitSignal_NilApplication(t *testing.T) {
+	// Create a nil logger instance.
+	var log *slog.Logger
+
+	// Create a nil application instance.
+	var application *app.App
+
+	// Create a channel to receive OS signals.
+	stop := make(chan os.Signal, 1)
+
+	// Simulate SIGINT signal.
+	signal.Notify(stop, syscall.SIGINT)
+
+	// Start the waitSignal function in a separate goroutine.
+	go func() {
+		waitSignal(log, application)
+	}()
+
+	// Send SIGINT signal to the waitSignal function.
+	stop <- syscall.SIGINT
+
+	// Wait for the waitSignal function to finish.
+	time.Sleep(100 * time.Millisecond)
+
+	// Verify that the waitSignal function did not panic.
+	if r := recover(); r != nil {
+		t.Fatalf("waitSignal function panicked: %v", r)
 	}
 }
