ac61a963f8089e7026db87a89457fbed2267f3b4
ajugalushkin <ajugalushkin@gmail.com>
2024-09-21 18:10:04 +0300

    fix: fix test

--- client/cmd/keep/update/text/keep_update_text_test.go
+++ client/cmd/keep/update/text/keep_update_text_test.go
@@ -200,3 +200,39 @@ func TestKeeperUpdateTextCmdRunE_Error(t *testing.T) {
 	// Assert
 	assert.Error(t, err)
 }
+
+func TestKeeperUpdateTextCmdRunE_Error2(t *testing.T) {
+	initClient(nil)
+	initCipher(nil)
+	token_cache.InitTokenStorage("")
+
+	// Arrange
+	logger.InitLogger(slog.New(slog.NewJSONHandler(os.Stdout, nil)),
+		&config.Config{Env: "dev"})
+
+	// Set up the context and logger
+	name := "non-existent-secret"
+	data := "test-data"
+
+	// Mock dependencies
+	token_cache.InitTokenStorage("token.txt")
+	err := token_cache.GetToken().Save("test-token")
+	assert.NoError(t, err)
+
+	config.InitConfig(&config.Config{Client: config.Client{Address: "test-address"}})
+
+	mockCipher := mocks.NewCipher(t)
+	mockCipher.On("Encrypt", mock.Anything, mock.Anything).Return(nil, fmt.Errorf("failed to encrypt data"))
+	initCipher(mockCipher)
+
+	// Create a command and set up flags
+	cmd := &cobra.Command{}
+	cmd.Flags().String("name", name, "secret name")
+	cmd.Flags().String("data", data, "secret data")
+
+	// Act
+	err = keeperUpdateTextCmdRunE(cmd, nil)
+
+	// Assert
+	assert.Error(t, err)
+}
